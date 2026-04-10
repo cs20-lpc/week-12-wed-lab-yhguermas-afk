27b60f84dc7c6f498b2f54f1400a882a92ae3d66
Yanni Guermas <yanniguermas@Mac.attlocal.net>
2026-04-09 22:58:12 -0700

    Week 12 Wed Lab

--- DoubleHashing.hpp
+++ DoubleHashing.hpp
@@ -8,7 +8,7 @@ public:
         : HashTableClosed<T>(size) {}
 
     int hash2(const T& key) const {
-        return 7 - (static_cast<int>(key) % 7);
+        return 1 + (static_cast<int>(key) % (this->M - 1));
     }
 
     int probeIndex(const T& key, int i) const override {
